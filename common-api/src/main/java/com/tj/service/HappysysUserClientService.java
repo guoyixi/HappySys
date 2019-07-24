81a05a44fb4ca79b5c299e774675d70053c00a93
startlt <2496627014@qq.com>
2019-07-24 16:58:37 +0800

    lt:fegin里的方法写了注解

--- common-api/src/main/java/com/tj/service/HappysysUserClientService.java
+++ common-api/src/main/java/com/tj/service/HappysysUserClientService.java
@@ -6,12 +6,43 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @FeignClient("HAPPY-USER")
 public interface HappysysUserClientService {
+    /**
+     * 添加用户
+     * @param user
+     * @return
+     */
     @RequestMapping("/add/user")
     public boolean addUser(HappysysUser user);
 
+    /**
+     * 根据条件username和password,返回count(*)
+     * @param user
+     * @return
+     */
     @RequestMapping("/login/user")
     public int login(HappysysUser user);
 
+    /**
+     * 根据条件username和password,返回一个对象
+     * @param user
+     * @return
+     */
     @RequestMapping("select/user")
     public HappysysUser showUser(HappysysUser user);
+
+    /**
+     * 根据usernmae判断是否存在，返回count(*)
+     * @param phone
+     * @return
+     */
+    @RequestMapping("user/nameisExists")
+    public int isexists(String phone);
+
+    /**
+     * 根据条件username找对象
+     * @param username
+     * @return
+     */
+    @RequestMapping("user/findbyname")
+    public HappysysUser findbyname(String username);
 }
