48906dc6d81c227158312904f21c50bf23cf6b6e
xiaoll1 <1197578609@qq.com>
2019-08-02 10:27:51 +0800

    xll：修改实体类字段，添加订单列表分页查询

--- consumer-pre/src/main/java/com/tj/product/controller/ProductController.java
+++ consumer-pre/src/main/java/com/tj/product/controller/ProductController.java
@@ -7,10 +7,7 @@ import com.tj.service.HappysysProductClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.HashMap;
