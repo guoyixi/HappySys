ce213354dbd8c5c6c5ce725e7c959b5c04dde822
guoyixi <1804099268@qq.com>
2019-08-07 16:34:32 +0800

    GUOYIXI is Crazy

--- consumer-pre/src/main/java/com/tj/product/controller/PaymentController.java
+++ consumer-pre/src/main/java/com/tj/product/controller/PaymentController.java
@@ -1,5 +1,6 @@
 package com.tj.product.controller;
 
+import com.tj.product.HappysysApplicantInfo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,18 +17,36 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class PaymentController {
 
- @RequestMapping(value = "/HappysysPayment/payment1/{product}/{totolPrice}/{deadline}/{insurance}",method = RequestMethod.GET)
- public String payment(@PathVariable("product") String product,@PathVariable("totolPrice") Double totolPrice,@PathVariable("deadline") String deadline,@PathVariable("insurance") String insurance,  HttpSession session){
-
-  System.out.println(product+totolPrice+deadline+insurance);
+ @RequestMapping(value = "/HappysysPayment/payment1/{product}/{totolPrice}/{section}/{deadline}/{insurance}",method = RequestMethod.GET)
+ public String paymentOne(@PathVariable("product") String product,@PathVariable("totolPrice") Double totolPrice,@PathVariable("section") String section,@PathVariable("deadline") String deadline,@PathVariable("insurance") String insurance,  HttpSession session){
 
 /*  Object user = session.getAttribute("user");
   if(user==null){
    return "redirect:/login.html";
   }*/
+ //判断User外键 !=null 查询投保人
+
+
+  //Ajax 查询 被保人列表
   return "product_payment1.html";
  }
 
+
+ @RequestMapping(value = "/HappysysPayment/payment2",method = RequestMethod.POST)
+ public String paymentTwo(HappysysApplicantInfo sdinformationAppnt, HappysysApplicantInfo sdinformationinsured,HttpSession session) {
+
+
+
+  System.out.println("/HappysysPayment/payment2:"+sdinformationAppnt+"\t"+sdinformationinsured);
+  //判断User外键 ==null 新增投保人/修改用户表外键
+
+  //新增被保人 applicant_info  apllcatn_info_center
+
+  //订单
+
+
+  return "product_payment2.html";
+ }
 
 
 }
